9f5c4a722cda61fd1bd7e3a59e048240c938d0d4
warnov <warnov@gmail.com>
2025-10-24 02:28:39 -0500

    aoai-agent finished ok

---
+++ aoai-agent/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.AI.OpenAI;
+using OpenAI;
+
+string endpoint = Environment.GetEnvironmentVariable("ENDPOINT")!;
+string apiKey = Environment.GetEnvironmentVariable("API_KEY")!;
+string model = Environment.GetEnvironmentVariable("MODEL")!;
+
+AzureOpenAIClient client = new(
+ new Uri(endpoint),
+ new Azure.AzureKeyCredential(apiKey)
+ );
+
+var agent = client.GetChatClient(model).CreateAIAgent();
+//var response = await agent.RunAsync("Según twitter Quien es WarNov?");
+//Console.WriteLine(response);
+
+await foreach(var update in agent.RunStreamingAsync("Cómo se hace un ajiaco??"))
+{
+ Console.Write(update);
+}
