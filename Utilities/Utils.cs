19ba13427ef47a1c802c8c5f2856b3d674329718
warnov <warnov@gmail.com>
2025-10-29 21:14:20 -0500

    Added token count functionality also with streaming token count sample in Utilities

--- Utilities/Utils.cs
+++ Utilities/Utils.cs
@@ -17,6 +17,34 @@ namespace Utilities
             {
                 Console.Write(update);
             }
-        }        
+        }
+
+        public static async Task AgentUsageSampleTokensCount(ChatClientAgent agent, string fullResponsePrompt, string streamingResponseSample, string agentType, AgentThread? thread = null)
+        {
+            Console.WriteLine($"--- {agentType} Agent Sample ---\n\n");
+
+            //Full response sample
+            var response = await agent.RunAsync(fullResponsePrompt, thread);
+            Console.WriteLine(response);
+            Console.WriteLine("-----");
+            //Streaming response sample
+            List<AgentRunResponseUpdate> updates = new();
+            await foreach (var update in agent.RunStreamingAsync(streamingResponseSample, thread))
+            {
+                updates.Add(update);
+                Console.Write(update);
+            }
+            var collectedResponseFromStreaming = updates.ToAgentRunResponse();
+            Console.WriteLine($"\nFull response tokens used: {response.Usage?.TotalTokenCount}");
+            Console.WriteLine($"Streaming Output tokens used: {collectedResponseFromStreaming.Usage?.OutputTokenCount}");
+
+            var additionalCounts = collectedResponseFromStreaming.Usage?.AdditionalCounts;
+            string reasoningTokenCount = "N/A";
+            if (additionalCounts != null && additionalCounts.TryGetValue("OutputTokenDetails.ReasoningTokenCount", out var value))
+            {
+                reasoningTokenCount = value.ToString();
+            }
+            Console.WriteLine($"Streaming Reasoning Output tokens used: {reasoningTokenCount}");
+        }
     }
 }
--- aoai-agent/aoai-agent-program.cs
+++ aoai-agent/aoai-agent-program.cs
@@ -15,10 +15,10 @@ var agent = client.GetChatClient(model).CreateAIAgent();
 //var response = await agent.RunAsync("Según twitter Quien es WarNov?");
 //Console.WriteLine(response);
 
-//await foreach (var update in agent.RunStreamingAsync("Cómo se hace una bandeja paisa??"))
-//{
-//    Console.Write(update);
-//}
+await foreach (var update in agent.RunStreamingAsync("Cómo se hace una bandeja paisa??"))
+{
+    Console.Write(update);
+}
 
 await Utils.AgentUsageSample(
     agent,
